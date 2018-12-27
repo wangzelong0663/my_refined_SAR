13ded19f72a8c5e1b79e165b31671d0cf48198a0
Zelong <hebe0663>
2018-12-27 19:38:00 +0800

    modified config

--- config.py
+++ config.py
@@ -5,14 +5,4 @@ Created on Wed Nov 21 21:56:43 2018
 
 @author: longer
 """
-
-IMAGE_SIZE = 128
-filters_size = 16
-lr = 1e-5
-MAX_STEP = 10000
-checkpoint_dir = './save_model'
-image_path = './combined_15/'
-batch_size = 64
-LAMBDA = 100
-
 path = './output_image'
